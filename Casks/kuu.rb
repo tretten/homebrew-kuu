d94f35f04d74d8682819659891975834ca0397ea
tretten <tretten@users.noreply.github.com>
2026-09-18 15:27:35 -0700

    kuu 0.8.9: fixed zip sha256

--- Casks/kuu.rb
+++ Casks/kuu.rb
@@ -1,6 +1,6 @@
 cask "kuu" do
   version "0.8.9"
-  sha256 "a5555b595bbb31aa13aa0547f388778c539a9fe44482a5476124fe12f1836318"
+  sha256 "3d661f2e642e70a67d9354373b1f82a4f25f0223246f50bd61c41046f3931652"
 
   url "https://github.com/tretten/kuu/releases/download/v#{version}/Kuu-#{version}.zip"
   name "Kuu"
