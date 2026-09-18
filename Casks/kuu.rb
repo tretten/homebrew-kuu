d4edcb8c1794e1d9edde4a107fda20cb8691f7ce
tretten <tretten@users.noreply.github.com>
2026-09-18 14:56:42 -0700

    kuu 0.8.8: versioned zip url

--- Casks/kuu.rb
+++ Casks/kuu.rb
@@ -2,7 +2,7 @@ cask "kuu" do
   version "0.8.8"
   sha256 "bfccb789f69e638adcef378740aaf1e0e1530c75f9642a85bb7ebef058905fed"
 
-  url "https://github.com/tretten/kuu/releases/download/v#{version}/Kuu-macos.zip"
+  url "https://github.com/tretten/kuu/releases/download/v#{version}/Kuu-#{version}.zip"
   name "Kuu"
   desc "Minimal native macOS terminal"
   homepage "https://github.com/tretten/kuu"
