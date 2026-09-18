8fd33d1291972844fdccee34d2bae8a96c5cda4c
tretten <tretten@users.noreply.github.com>
2026-09-18 07:55:14 -0700

    Add kuu cask (placeholder version, filled by release workflow)

---
+++ Casks/kuu.rb
@@ -0,0 +1,15 @@
+cask "kuu" do
+  version "0.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/tretten/kuu/releases/download/v#{version}/Kuu-macos.zip"
+  name "Kuu"
+  desc "Minimal native macOS terminal"
+  homepage "https://github.com/tretten/kuu"
+
+  app "Kuu.app"
+
+  postflight do
+    system_command "xattr", args: ["-dr", "com.apple.quarantine", "#{appdir}/Kuu.app"]
+  end
+end
