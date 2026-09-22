1e8278a65727a2e15d85cb2d7a51144cea92e912
tretten <tretten@users.noreply.github.com>
2026-09-21 23:11:17 -0700

    kuu 0.8.13

--- Casks/kuu.rb
+++ Casks/kuu.rb
@@ -1,6 +1,6 @@
 cask "kuu" do
-  version "0.8.12"
-  sha256 "4d435a5b9480d5d3601b3765fc5abfc80e390e3b492b2a003895ac26f3d2d813"
+  version "0.8.13"
+  sha256 "9853ccec85a2311b36ab8577f70cdb84a3f0bcd49b2c4a9acf3a0ea3be2751f4"
 
   url "https://github.com/tretten/kuu/releases/download/v#{version}/Kuu-#{version}.zip"
   name "Kuu"
